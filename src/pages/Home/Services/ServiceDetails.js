9fc1d7ce75d4cb6aa5ca4e20da84369350e71a7f
Sheikh sadi <you@example.com>
2022-11-10 11:49:06 +0600

    service details page added

--- src/pages/Home/Services/ServiceDetails.js
+++ src/pages/Home/Services/ServiceDetails.js
@@ -3,10 +3,20 @@ import { useLoaderData } from 'react-router-dom';
 
 const ServiceDetails = () => {
      const service = useLoaderData();
-     const {title} = service;
+     const {title, position,price,description,img,_id } = service;
      return (
-          <div>
-               {title}
+          <div className='max-w-screen-lg mx-auto my-3 rounded shadow-lg sm:h-auto'>
+               <section className='grid gap-3 grid-cols-1 md:grid-cols-3 mx-2'>
+                    <div className='h-72'>
+                         <img className='h-72 w-full object-cover rounded' src={img} alt="" />
+                    </div>
+                    <div className='col-span-2'>
+                         <p className='text-3xl font-bold'>{title}({position})</p>
+                         <p className='my-3'>{description}</p>
+                         <p className='text-lg font-semibold'>price: ${price}</p>
+                    </div>
+
+               </section>
           </div>
      );
 };
